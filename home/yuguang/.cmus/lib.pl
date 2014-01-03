0fba8624a1465d5f6f8e30e9702bd44f24c7616b
yuguang <zh.yuguang.cn@gmail.com>
2014-01-03 15:57:16 -0500

    modify music library

--- home/yuguang/.cmus/lib.pl
+++ home/yuguang/.cmus/lib.pl
@@ -1,56 +1,56 @@
-/mnt/media/Music/Eckhart Tolle/Music for Inner Stillness/01 - Various Artists - Kirtas Hildegard of Bingen.flac
-/mnt/media/Music/Eckhart Tolle/Music for Inner Stillness/02 - Various Artists - Searching Shirabe.flac
 /mnt/media/Music/Eckhart Tolle/Music for Inner Stillness/03 - Various Artists - For as Long as Space Endures.flac
+/mnt/media/Music/Eckhart Tolle/Music for Inner Stillness/02 - Various Artists - Searching Shirabe.flac
+/mnt/media/Music/Eckhart Tolle/Music for Inner Stillness/11 - Various Artists - Ocean.flac
 /mnt/media/Music/Eckhart Tolle/Music for Inner Stillness/04 - Various Artists - A Thousand Whispers.flac
 /mnt/media/Music/Eckhart Tolle/Music for Inner Stillness/05 - Various Artists - The Song of the Universal Light - Part 1.flac
-/mnt/media/Music/Eckhart Tolle/Music for Inner Stillness/06 - Various Artists - The Song of the Universal Light - Part 2.flac
 /mnt/media/Music/Eckhart Tolle/Music for Inner Stillness/07 - Various Artists - Epiphany.flac
 /mnt/media/Music/Eckhart Tolle/Music for Inner Stillness/08 - Various Artists - Molecule 5.flac
+/mnt/media/Music/Eckhart Tolle/Music for Inner Stillness/06 - Various Artists - The Song of the Universal Light - Part 2.flac
 /mnt/media/Music/Eckhart Tolle/Music for Inner Stillness/09 - Various Artists - Hawks Circling.flac
+/mnt/media/Music/Eckhart Tolle/Music for Inner Stillness/01 - Various Artists - Kirtas Hildegard of Bingen.flac
 /mnt/media/Music/Eckhart Tolle/Music for Inner Stillness/10 - Various Artists - Synchronicity High Tech Meditation Musical Transcendence.flac
-/mnt/media/Music/Eckhart Tolle/Music for Inner Stillness/11 - Various Artists - Ocean.flac
-/mnt/media/Music/Eckhart Tolle/Music to Quiet the Mind/01 Deva Premal - Incantation.flac
 /mnt/media/Music/Eckhart Tolle/Music to Quiet the Mind/02 Kip Mazuy - Now is Forever.flac
-/mnt/media/Music/Eckhart Tolle/Music to Quiet the Mind/03 Jeff Johnson - Heavens Door.flac
-/mnt/media/Music/Eckhart Tolle/Music to Quiet the Mind/04 Briggen Ghezali feat Praful-Gnoss.flac
-/mnt/media/Music/Eckhart Tolle/Music to Quiet the Mind/05 David Sun - Joyful Spirit.flac
-/mnt/media/Music/Eckhart Tolle/Music to Quiet the Mind/06 Benjy Wertheimer - Threshold Between Worlds.flac
-/mnt/media/Music/Eckhart Tolle/Music to Quiet the Mind/07 Deva Premal - Om Hraum Mitraya.flac
-/mnt/media/Music/Eckhart Tolle/Music to Quiet the Mind/08 Nawang Khechog - Wisdom and Compassion.flac
-/mnt/media/Music/Eckhart Tolle/Music to Quiet the Mind/09 Martin Jones - Clair de Lune.flac
 /mnt/media/Music/Eckhart Tolle/Music to Quiet the Mind/10 Steve Roach - Structures from Silence excerpt.flac
+/mnt/media/Music/Eckhart Tolle/Music to Quiet the Mind/04 Briggen Ghezali feat Praful-Gnoss.flac
+/mnt/media/Music/Eckhart Tolle/Music to Quiet the Mind/03 Jeff Johnson - Heavens Door.flac
 /mnt/media/Music/Eckhart Tolle/Music to Quiet the Mind/11 Maneesh de Moor - Let it Be.flac
-/mnt/media/Music/Erik Satie/Pascal Rogé/01 - Gymnopédie No. 1.flac
-/mnt/media/Music/Erik Satie/Pascal Rogé/02 - Gymnopédie No. 2.flac
-/mnt/media/Music/Erik Satie/Pascal Rogé/03 - Gymnopédie No. 3.flac
-/mnt/media/Music/Erik Satie/Pascal Rogé/04 - Gnossienne No. 1.flac
-/mnt/media/Music/Erik Satie/Pascal Rogé/05 - Gnossienne No. 2.flac
-/mnt/media/Music/Erik Satie/Pascal Rogé/06 - Gnossienne No. 3.flac
-/mnt/media/Music/Erik Satie/Pascal Rogé/07 - Gnossienne No. 4.flac
-/mnt/media/Music/Erik Satie/Pascal Rogé/08 - Gnossienne No. 5.flac
+/mnt/media/Music/Eckhart Tolle/Music to Quiet the Mind/01 Deva Premal - Incantation.flac
+/mnt/media/Music/Eckhart Tolle/Music to Quiet the Mind/07 Deva Premal - Om Hraum Mitraya.flac
+/mnt/media/Music/Eckhart Tolle/Music to Quiet the Mind/09 Martin Jones - Clair de Lune.flac
+/mnt/media/Music/Eckhart Tolle/Music to Quiet the Mind/08 Nawang Khechog - Wisdom and Compassion.flac
+/mnt/media/Music/Eckhart Tolle/Music to Quiet the Mind/06 Benjy Wertheimer - Threshold Between Worlds.flac
+/mnt/media/Music/Eckhart Tolle/Music to Quiet the Mind/05 David Sun - Joyful Spirit.flac
 /mnt/media/Music/Erik Satie/Pascal Rogé/09 - Gnossienne No. 6.flac
-/mnt/media/Music/Erik Satie/Pascal Rogé/10 - Nocturne I.flac
-/mnt/media/Music/Erik Satie/Pascal Rogé/11 - Nocturne II.flac
-/mnt/media/Music/Erik Satie/Pascal Rogé/12 - Nocturne III.flac
-/mnt/media/Music/Erik Satie/Pascal Rogé/13 - Nocturne IV.flac
-/mnt/media/Music/Erik Satie/Pascal Rogé/14 - Nocturne V.flac
-/mnt/media/Music/Erik Satie/Pascal Rogé/15 - Avant-dernieres pensées (I Idylle, à Debussy, II Aubade, à Paul Dukas, III Meditation, à Albert Roussel).flac
-/mnt/media/Music/Erik Satie/Pascal Rogé/16 - Pieces froides - Trois airs à fuir.flac
-/mnt/media/Music/Erik Satie/Pascal Rogé/17 - Pieces froides - Trois danses de travers.flac
-/mnt/media/Music/Erik Satie/Pascal Rogé/18 - Deux reveries nocturnes.flac
 /mnt/media/Music/Erik Satie/Pascal Rogé/19 - Prélude de la porte héroïque du ciel.flac
-/mnt/media/Music/Songs of Mary/01 Virgen Madre Groriosa (Cantigas de Santa Maria).flac
+/mnt/media/Music/Erik Satie/Pascal Rogé/05 - Gnossienne No. 2.flac
+/mnt/media/Music/Erik Satie/Pascal Rogé/01 - Gymnopédie No. 1.flac
+/mnt/media/Music/Erik Satie/Pascal Rogé/16 - Pieces froides - Trois airs à fuir.flac
+/mnt/media/Music/Erik Satie/Pascal Rogé/03 - Gymnopédie No. 3.flac
+/mnt/media/Music/Erik Satie/Pascal Rogé/17 - Pieces froides - Trois danses de travers.flac
+/mnt/media/Music/Erik Satie/Pascal Rogé/15 - Avant-dernieres pensées (I Idylle, à Debussy, II Aubade, à Paul Dukas, III Meditation, à Albert Roussel).flac
+/mnt/media/Music/Erik Satie/Pascal Rogé/11 - Nocturne II.flac
+/mnt/media/Music/Erik Satie/Pascal Rogé/13 - Nocturne IV.flac
+/mnt/media/Music/Erik Satie/Pascal Rogé/18 - Deux reveries nocturnes.flac
+/mnt/media/Music/Erik Satie/Pascal Rogé/12 - Nocturne III.flac
+/mnt/media/Music/Erik Satie/Pascal Rogé/14 - Nocturne V.flac
+/mnt/media/Music/Erik Satie/Pascal Rogé/06 - Gnossienne No. 3.flac
+/mnt/media/Music/Erik Satie/Pascal Rogé/02 - Gymnopédie No. 2.flac
+/mnt/media/Music/Erik Satie/Pascal Rogé/08 - Gnossienne No. 5.flac
+/mnt/media/Music/Erik Satie/Pascal Rogé/10 - Nocturne I.flac
+/mnt/media/Music/Erik Satie/Pascal Rogé/04 - Gnossienne No. 1.flac
+/mnt/media/Music/Erik Satie/Pascal Rogé/07 - Gnossienne No. 4.flac
 /mnt/media/Music/Songs of Mary/02 Quant voi la flor novele (Anonymous).flac
-/mnt/media/Music/Songs of Mary/03 Magnificat (Tomas Pascual).flac
-/mnt/media/Music/Songs of Mary/04 Ave Maris Stella (Traditional).flac
-/mnt/media/Music/Songs of Mary/05 Ave Generosa (Hildegard of Bingen).flac
-/mnt/media/Music/Songs of Mary/06 Karitas (Hildegard of Bingen).flac
-/mnt/media/Music/Songs of Mary/07 Santa Maria, Strela Do Dia (Cantigas de Santa Maria).flac
-/mnt/media/Music/Songs of Mary/08 Sciathluireach Mhuire (Traditional Celtic).flac
-/mnt/media/Music/Songs of Mary/09 Ave Maria (Anonymous).flac
 /mnt/media/Music/Songs of Mary/10 Alma Redemptoris Mater (Traditional Gregorian Chant).flac
-/mnt/media/Music/Songs of Mary/11 Santa Maria Looei, E Loo E Looarei (Cantigas de Santa Maria).flac
 /mnt/media/Music/Songs of Mary/12 O Clarissima Mater (Hildegard of Bingen).flac
-/mnt/media/Music/Songs of Mary/13 Hail Mary (Traditional, adapted).flac
-/mnt/media/Music/Songs of Mary/14 Salve (Hernando Franco).flac
+/mnt/media/Music/Songs of Mary/07 Santa Maria, Strela Do Dia (Cantigas de Santa Maria).flac
+/mnt/media/Music/Songs of Mary/09 Ave Maria (Anonymous).flac
+/mnt/media/Music/Songs of Mary/05 Ave Generosa (Hildegard of Bingen).flac
 /mnt/media/Music/Songs of Mary/15 O Virdissima Virga (Hildegard of Bingen).flac
+/mnt/media/Music/Songs of Mary/11 Santa Maria Looei, E Loo E Looarei (Cantigas de Santa Maria).flac
+/mnt/media/Music/Songs of Mary/13 Hail Mary (Traditional, adapted).flac
+/mnt/media/Music/Songs of Mary/06 Karitas (Hildegard of Bingen).flac
+/mnt/media/Music/Songs of Mary/08 Sciathluireach Mhuire (Traditional Celtic).flac
+/mnt/media/Music/Songs of Mary/03 Magnificat (Tomas Pascual).flac
+/mnt/media/Music/Songs of Mary/01 Virgen Madre Groriosa (Cantigas de Santa Maria).flac
+/mnt/media/Music/Songs of Mary/14 Salve (Hernando Franco).flac
+/mnt/media/Music/Songs of Mary/04 Ave Maris Stella (Traditional).flac
